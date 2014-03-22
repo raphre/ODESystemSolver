3d39b1c9389a316405f496244944fcccd9158692
Raphael Reyna <raphre@gmail.com>
2014-03-22 16:48:35 -0700

    Initial Commit

---
+++ 402P2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  402P2
+//
+//  Created by Raphael Reyna on 3/22/14.
+//  Copyright (c) 2014 me. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
